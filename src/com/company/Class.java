3d8ad08bce44846a0b9871fa4adc47d0a70d1358
G. Ray <gray-venom@bk.ru>
2021-11-30 12:36:43 +0300

    init

---
+++ src/com/company/Class.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Class {
+    public String Number;
+    public Teacher ClassTeacher;
+    public Learner[] Learners;
+
+    public Class(String N, Teacher T, Learner[] L)
+    {
+        Number = N;
+        ClassTeacher = T;
+        Learners = L;
+    }
+
+    public void getList()
+    {
+        //какой-то код
+    }
+
+    public void getListParents()
+    {
+        //какой-то код
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Class class11 = new Class("11", new Teacher(), new Learner[]{new Learner()});
+        Learner l1 = new Learner();
+    }
+}
